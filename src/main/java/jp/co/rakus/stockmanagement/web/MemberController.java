1b1c8cd31a21680045587b285670f27ec1d83095
atsukoR <atsuko.yoshino@rakus.co.jp>
2017-08-14 14:46:01 +0900

    8月14日：メンバー登録後にログイン画面表示が行われるように変更しました。

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
@@ -50,13 +50,15 @@ public class MemberController {
 	 * @param model モデル
 	 * @return ログイン画面
 	 */
+	@SuppressWarnings("null")
 	@RequestMapping(value = "create")
 	public String create(@Validated MemberForm form, 
 			Model model) {
 		Member member = new Member();
 		BeanUtils.copyProperties(form, member);
 		memberService.save(member);
-		return "book/list";
+		
+		return "redirect:/";
 	}
 	
 }
